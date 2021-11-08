75861da6e7cdf0a2ff51b04e785a32bd3e4ec82a
Grégoire Houssard <gregoire.houssard@akeneo.com>
2021-11-08 13:26:06 +0100

    Remove keep app awake cronjob

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,6 @@
 import { env } from 'process';
 import { CronJob } from 'cron';
 import { postAggregatedWalletInRabChannel, startApp } from './slack/app';
-import axios from 'axios';
 
 const registerCronJobs = (): void => {
   const cronTimezone: string = env.CRON_TIMEZONE || '';
@@ -21,24 +20,6 @@ const registerCronJobs = (): void => {
     );
 
     console.info('Frequent reporting enabled');
-  }
-
-  const keepAppAwake: boolean = 'true' === env.KEEP_APP_AWAKE;
-  if(keepAppAwake) {
-    const keepAppAwakeCronTime: string = env.KEEP_APP_AWAKE_CRON_TIME || '';
-    const appUrl: string = env.APP_URL || '';
-
-    new CronJob(
-      keepAppAwakeCronTime, 
-      async (): Promise<void> => {
-        await axios.get(appUrl);
-      },
-      null,
-      true,
-      cronTimezone,
-    );
-
-    console.info('Keep app awake enabled');
   }
 };
 
