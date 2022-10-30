b879373820a5099bed3df0ba15ca36ee840af070
Grégoire Houssard <ghoussard@protonmail.com>
2022-10-30 08:38:39 +0100

    Create a simple function using v2 API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,30 @@
-import * as functions from "firebase-functions";
+import { onRequest } from "firebase-functions/v2/https";
+import { logger } from "firebase-functions/v2";
+import { defineString } from "firebase-functions/params";
+import { App, ExpressReceiver } from "@slack/bolt";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+const signingSecret = defineString("SLACK_SIGNING_SECRET");
+const botToken = defineString("SLACK_BOT_TOKEN");
+
+export const handleslackcommand = onRequest((request, response) => {
+  logger.info("Slack event received");
+
+  const receiver = new ExpressReceiver({
+    endpoints: "/slack/events",
+    signingSecret: signingSecret.value(),
+  });
+
+  const app = new App({
+    receiver,
+    token: botToken.value(),
+    signingSecret: signingSecret.value(),
+  });
+
+  app.command("/rab-wallet", async ({ ack, respond }) => {
+    await ack();
+
+    await respond("RAB Wallet is not available for now! :(");
+  });
+
+  receiver.requestHandler(request, response);
 });
